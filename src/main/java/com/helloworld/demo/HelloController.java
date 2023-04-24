d66a08d80cb0fb1ea7f1d6804d92d982994efe58
Zimisele Madani <ZimiseleM@blts.co.za>
2023-04-24 08:15:50 +0200

    first commit

---
+++ src/main/java/com/helloworld/demo/HelloController.java
@@ -0,0 +1,12 @@
+package com.helloworld.demo;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    @RequestMapping("/hello")
+    public String Hello(){
+        return "Spring is here!";
+    }
+}
